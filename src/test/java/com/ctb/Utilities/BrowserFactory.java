5632cb345ff5c0b339ada50cf650c9a744f4386c
dglushko <darinaglushko@gmail.com>
2020-05-19 09:50:21 -0400

    fist commit

---
+++ src/test/java/com/ctb/Utilities/BrowserFactory.java
@@ -0,0 +1,16 @@
+package com.ctb.Utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class BrowserFactory {
+
+	public static WebDriver getDriver(String browser) {
+		
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		String os = System.getProperty("os.name");
+		System.out.println(os);
+	}
+}
